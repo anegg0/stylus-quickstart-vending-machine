5ac2716babfdb2e30bd3010ade8ac70c7616455f
Gaël Blanchemain <gael.blanchemain@protonmail.com>
2025-04-19 10:39:21 -0700

    refactor: update entrypoint for compatibility with test file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-#![cfg_attr(not(feature = "export-abi"), no_main)]
+#![cfg_attr(not(any(test, feature = "export-abi")), no_main)]
+
+#[cfg(not(any(test, feature = "export-abi")))]
+#[no_mangle]
+pub extern "C" fn main() {}
 
 #[cfg(feature = "export-abi")]
 fn main() {
-    stylus_cupcake_example::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
+    stylus_hello_world::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
 }
